e79502b735e3a058f15f0eb3fa29104b97cd7857
skaa1310 <Anders skaa1310@stthomas.edu>
2018-02-03 12:24:53 -0600

    my first commit

--- 635_craps_simulation/src/TestCraps.java
+++ 635_craps_simulation/src/TestCraps.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+// my first git commit
+
 /**
  * Starting application for running multiple simulations of CrapsGame. This
  * gathers statistics (how many games were a win, and how many were a loss), and
